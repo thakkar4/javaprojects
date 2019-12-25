20de1a60749228a95e3b17cc665d65b7a5df6197
thakkar4 <57274132+thakkar4@users.noreply.github.com>
2019-12-25 15:36:40 -0600

    Initiate file

---
+++ javaproject/src/javaproject/Biggest.java
@@ -0,0 +1,27 @@
+package javaproject;
+
+//Write a program to find biggest among 5 numbers;
+public class Biggest{
+
+public static void main (String [] args) {
+
+		int a = 23;
+		int b = 19;
+		int c = 21;
+		int d = 35;
+		int e = 38;
+
+		if (a>b && a>c&& a>d && a>e)
+		System.out.println(" Biggest is : " + a); 
+		else if (b>c && b>a && b>d && b>e)
+		System.out.println("Biggest is: " + b);
+		else if (c>d)
+		System.out.println("Biggest is : " +c);	
+		else if (d>e)
+		System.out.println("Biggest is : " +d);	
+		else
+		System.out.println("Biggest is : " +d);
+		
+
+}
+}
